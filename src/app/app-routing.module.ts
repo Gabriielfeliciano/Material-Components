60d256f0f62dcfc31b23706dd1295c74c674c392
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-11-07 16:05:02 -0300

    New modules and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { ButtonsComponent } from './pages/buttons/buttons.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TreeComponent } from './pages/tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'buttons',
+    component: ButtonsComponent
+  },
+  {
+    path:'tree',
+    component: TreeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
